ea54f5ee38444d373b33890ca7476be8166120c2
zayed <zayed@sgcsoft.net>
2022-03-07 15:44:55 +0600

    Disable preference fetching in table component

--- apps/admin/src/packages/services/Api/Queries/AdminQueries/Preferences.ts
+++ apps/admin/src/packages/services/Api/Queries/AdminQueries/Preferences.ts
@@ -1,25 +1,26 @@
-import { endpoints } from "~/packages/services/Api/Queries/AdminQueries/Endpoints"
+//import { endpoints } from "~/packages/services/Api/Queries/AdminQueries/Endpoints"
 import { adminApi } from "~/packages/services/Api/ApiClient"
 import { IQuery, PreferenceQuery } from "~/packages/services/Api/Queries/AdminQueries/types";
 
 export const PreferenceQueries:IQuery<PreferenceQuery> = {
   getPreferences: async (data: any) => {
-    return adminApi({
-      endpoint: `${endpoints.CART}/${data!.params!.id}`,
-      ...data,
-      method: "GET"
+    return Promise.resolve({
+      code: 200,
+      error: null,
+      data: null,
+      success: true,
     })
   },
   saveOrUpdatePreferences: async (data: any) => {
     return adminApi({
-      endpoint: `${endpoints.CART}/${data!.params!.id}`,
+      endpoint: `${'endpoints.PREFERENCE'}/${data!.params!.id}`,
       ...data,
       method: "GET"
     })
   },
   deletePreferences: async (data: any) => {
     return adminApi({
-      endpoint: `${endpoints.CART}/${data!.params!.id}`,
+      endpoint: `${'endpoints.PREFERENCE'}/${data!.params!.id}`,
       ...data,
       method: "GET"
     })
