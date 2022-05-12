02b99ad8d6e84b6b473fd830dba9640a82e5ad55
TofaH <hasan.tofa@gmail.com>
2022-05-12 09:47:51 +0600

    feature: role edit

--- apps/admin/src/TableSearchMeta/Role/RoleDetailsMeta.tsx
+++ apps/admin/src/TableSearchMeta/Role/RoleDetailsMeta.tsx
@@ -1,34 +1,34 @@
-// import { message } from "antd"
+import { message } from "antd"
 import { CardContainer, IDetailsSummary } from "~/packages/components/Page/DetailsPage/DetailsPageInterfaces"
 import { IDetailsMeta, IDetailsTabMeta } from "~/packages/components/Page/DetailsPage/Common"
-// import { QueryConstructor } from "~/packages/services/Api/Queries/AdminQueries/Proxy"
-// import { RoleQueries } from "~/packages/services/Api/Queries/AdminQueries/Roles"
-// import { UPDATE_SUCCESSFULLY } from "~/Constants"
-// import { MetaDrivenFormModalOpenButton } from "~/packages/components/Modal/MetaDrivenFormModal/MetaDrivenFormModalOpenButton"
+import { QueryConstructor } from "~/packages/services/Api/Queries/AdminQueries/Proxy"
+import { RoleQueries } from "~/packages/services/Api/Queries/AdminQueries/Roles"
+import { UPDATE_SUCCESSFULLY } from "~/Constants"
+import { MetaDrivenFormModalOpenButton } from "~/packages/components/Modal/MetaDrivenFormModal/MetaDrivenFormModalOpenButton"
 // import { RoleFormMeta } from "~/Component/Feature/Roles/FormMeta/RoleFormMeta"
-// import { REFRESH_PAGE } from "~/packages/utils/EventBus"
+import { REFRESH_PAGE } from "~/packages/utils/EventBus"
+import { getRoleUpdateFormMeta } from '~/Component/Feature/Roles/FormMeta/RoleUpdateFormMeta'
 
 export const getRoleDetailsMeta = (role: { [key: string]: any }): IDetailsMeta => {
-  // const updateEntity = QueryConstructor(((data) => RoleQueries.update({ ...data, params: { id: role.id } }).then(resp => {
-  //   if (resp.success) {
-  //     message.success(UPDATE_SUCCESSFULLY)
-  //   }
-  //   return resp
-  // })), [RoleQueries.update])
+  const updateEntity = QueryConstructor(((data) => RoleQueries.update({ ...data, params: { id: role.id } }).then(resp => {
+    if (resp.success) {
+      message.success(UPDATE_SUCCESSFULLY)
+    }
+    return resp
+  })), [RoleQueries.update])
 
   const summaryInfo: CardContainer = {
     title: `Role: ${role.name}`,
     cardActions: [
-      // <MetaDrivenFormModalOpenButton
-      //   formTitle={`Update Role`}
-      //   formMeta={RoleFormMeta}
-      //   formSubmitApi={updateEntity}
-      //   initialFormValue={{ ...role }}
-      //   defaultFormValue={{ roleId: role.id }}
-      //   buttonLabel={`Update Role`}
-      //   iconType="edit"
-      //   refreshEventName={REFRESH_PAGE}
-      // />,
+      <MetaDrivenFormModalOpenButton
+        formTitle={`Update ${role.name}`}
+        formMeta={getRoleUpdateFormMeta(role.id)}
+        initialFormValue={{ name: role.name, permissions: role.permissions.map((permission: any) => permission.id), menu_permissions: role.menu_permissions }}
+        formSubmitApi={updateEntity}
+        buttonLabel={`Update Role`}
+        iconType="edit"
+        refreshEventName={REFRESH_PAGE}
+      />
       // <ResourceRemoveLink ResourceID={Resource.ResourceID} />
     ],
     contents: [
@@ -45,7 +45,7 @@ export const getRoleDetailsMeta = (role: { [key: string]: any }): IDetailsMeta =
       tabTitle: "Summary",
       tabType: "summary",
       tabMeta: summaryMeta,
-      helpKey: "roleSummaryTab"
+      helpKey: "roleSummaryTab",
     },
   ]
 
