6c3fe537c5b1d61328d8ff93606af67f92641188
Zayed Ahmed <48876815+trulyzed@users.noreply.github.com>
2022-05-19 10:43:23 +0600

    Props updated in groupedList component

--- apps/admin/src/packages/components/Page/DetailsPage/GroupedList.tsx
+++ apps/admin/src/packages/components/Page/DetailsPage/GroupedList.tsx
@@ -4,15 +4,14 @@ import { Card, Col, Row, Tag } from "antd";
 interface IDetailsGroupedListProp {
   data: { [key: string]: any }[],
   groupKey: string,
-  titleKey: string,
+  displayKey: string,
 }
 
 export default function GroupedList(props: IDetailsGroupedListProp) {
-
   const {
     groupKey,
     data,
-    titleKey
+    displayKey
   } = props
 
   const processedData = data.reduce((a: { [key: string]: any }[], c) => {
@@ -33,10 +32,10 @@ export default function GroupedList(props: IDetailsGroupedListProp) {
       <Row gutter={16}>
         <Col span={24}>
           {
-            processedData.map((group: { [key: string]: any }, index: number) => (
-              <Card key={index} title={group.groupName} size={'small'} style={{ marginBottom: 4 }}>
+            processedData.map((group: { [key: string]: any }) => (
+              <Card key={group.groupName} title={group.groupName} size={'small'} style={{ marginBottom: 4 }}>
                 {
-                  group.data.map((dataItem: { [key: string]: any }, indx: number) => <Tag key={indx}>{dataItem[titleKey]}</Tag>)
+                  group.data.map((dataItem: { [key: string]: any }, index: number) => <Tag key={index}>{dataItem[displayKey]}</Tag>)
                 }
               </Card>
             ))
