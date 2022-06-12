e75a49e00a5a2956beaf0f1d92ab4f1be8a42f2e
zayed <zayed@sgcsoft.net>
2022-06-12 11:09:19 +0600

    [CP2-1983] - Use proper permission of tagged career list in course details

--- apps/admin/src/Pages/Institute/Courses/CourseDetailsPage.tsx
+++ apps/admin/src/Pages/Institute/Courses/CourseDetailsPage.tsx
@@ -3,26 +3,9 @@ import { DetailsPage } from "~/packages/components/Page/DetailsPage/DetailsPage"
 import { getCourseDetailsMeta } from "~/TableSearchMeta/Course/CourseDetailsMeta"
 import { CourseQueries } from "~/packages/services/Api/Queries/AdminQueries/Courses"
 import { QueryConstructor } from "~/packages/services/Api/Queries/AdminQueries/Proxy"
-import { CareerQueries } from "~/packages/services/Api/Queries/AdminQueries/Careers"
 
 export function CourseDetailsPage(props: RouteComponentProps<{ courseID?: string }>) {
   const CourseID = props?.match?.params?.courseID
 
-  const courseDetailsQuery = QueryConstructor(() => {
-    return Promise.all([CourseQueries.getSingle({ params: { id: CourseID } }), CareerQueries.getCareersAndSkillsByCourse({ params: { course_id: CourseID } })]).then(responses => {
-      const [resp1, resp2] = responses
-      return {
-        ...resp1,
-        ...resp2,
-        data: {
-          ...resp1.data,
-          tagged_careers: resp2.data.careers,
-          tagged_skills: resp2.data.skills
-        }
-      }
-    })
-  }, [CourseQueries.getSingle, CareerQueries.getCareersAndSkillsByCourse])
-
-
-  return <DetailsPage getMeta={getCourseDetailsMeta} getDetailsPageContent={courseDetailsQuery} entityType="course" entityID={CourseID} titleKey="name" />
+  return <DetailsPage getMeta={getCourseDetailsMeta} getDetailsPageContent={QueryConstructor(() => CourseQueries.getSingle({ params: { id: CourseID } }), [CourseQueries.getSingle])} entityType="course" entityID={CourseID} titleKey="name" />
 }
