dbd5ac3c744db976ae5edcba1ad70567d9dd9282
zayed <zayed@sgcsoft.net>
2022-09-07 11:52:53 +0600

    [CP2-2191] Modal with close action, modal top offset

--- packages/components/src/Modal/Modal.tsx
+++ packages/components/src/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import React, { CSSProperties, useEffect, useRef } from "react"
 import ReactDOM from "react-dom"
-import { Row, Col, Spin, Card } from "antd"
+import { Row, Col, Spin, Card, Grid } from "antd"
 import { zIndexLevel } from "~/zIndexLevel"
 import FocusTrap from "focus-trap-react"
 import { Options as FocusTrapOptions } from "focus-trap"
@@ -56,6 +56,7 @@ export function Modal({
   apiCallInProgress = false,
   closeModal
 }: IModalProp) {
+  const breakpoint = Grid.useBreakpoint()
   const modalID = generateUUID("modalContainer")
   const focusTrapOption = {
     allowOutsideClick: () => true,
@@ -112,7 +113,7 @@ export function Modal({
               <Col flex="auto"></Col>
               {loading && <ModalLoading {...{ width, loadingTip }} />}
               {!loading && (
-                <Col flex={width}>
+                <Col flex={width} style={breakpoint.md ? { top: "3rem" } : undefined}>
                   {children}
                   {apiCallInProgress && (
                     <div style={apiInProgressStyle}>
