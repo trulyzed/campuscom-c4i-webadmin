c3c639938931f037f6d9a904235899cac0c54a62
zayed <zayed@sgcsoft.net>
2022-09-12 15:10:14 +0600

    [CP2-2198] Accessibility issue - 7.No Data text contrast issue

--- packages/components/src/Layout/EmptyState.tsx
+++ packages/components/src/Layout/EmptyState.tsx
@@ -8,9 +8,9 @@ interface IEmptyStateProps {
 
 export const EmptyState = ({ message = "No Data", size = "default", wrapInCard }: IEmptyStateProps) => {
   const content = (
-    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", ...size === "default" && { margin: "20px 0" } }}>
-      <span style={{ fontSize: size === "small" ? "18px" : "40px", color: "#d9d9d9" }} className="glyphicon glyphicon-inbox" />
-      <p style={{ margin: 0, color: "#696969" }}>{message}</p>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", color: "#696969", ...size === "default" && { margin: "20px 0" } }}>
+      <span style={{ fontSize: size === "small" ? "18px" : "40px" }} className="glyphicon glyphicon-inbox" />
+      <p style={{ margin: 0 }}>{message}</p>
     </div>
   )
   return (
