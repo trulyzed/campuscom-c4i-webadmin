e3809b28bc6c947dbde3881f68cf009d152e2cb9
zayed <zayed@sgcsoft.net>
2022-09-29 13:16:48 +0600

    Rename create enrollment files to create order

--- apps/admin/src/routes.tsx
+++ apps/admin/src/routes.tsx
@@ -20,7 +20,7 @@ export const AppRoutes: RouteProps[] = [
   { path: "/store/product/:productID", component: React.lazy(() => import("~/Pages/Store/Products/ProductDetailsPage").then((x) => ({ default: x.ProductDetailsPage }))) },
   { path: "/store/payment-gateway/:paymentGatewayID", component: React.lazy(() => import("~/Pages/Store/PaymentGateways/StorePaymentGatewayDetailsPage").then((x) => ({ default: x.StorePaymentGatewayDetailsPage }))) },
   { path: "/store/configuration/:storeConfigurationID", component: React.lazy(() => import("~/Pages/Store/StoreConfigurations/StoreConfigurationDetailsPage").then((x) => ({ default: x.StoreConfigurationDetailsPage }))) },
-  { path: "/store/create-order", component: React.lazy(() => import("~/Pages/Administration/Enrollments/Create").then((x) => ({ default: x.Create }))) },
+  { path: "/store/create-order", component: React.lazy(() => import("~/Pages/Store/Orders/Create").then((x) => ({ default: x.Create }))) },
 
   { path: "/storefront-data/order", component: React.lazy(() => import("~/Pages/StorefrontData/Orders").then((x) => ({ default: x.List }))) },
   { path: "/storefront-data/order/:orderID", component: React.lazy(() => import("~/Pages/StorefrontData/Orders/OrderDetailsPage").then((x) => ({ default: x.OrderDetailsPage }))) },
