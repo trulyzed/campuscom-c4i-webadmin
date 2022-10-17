0bff7cebbf07ce408091aadef9bc96c31f0fabeb
zayed <zayed@sgcsoft.net>
2022-10-17 12:39:26 +0600

    Change argument from string to ReactNode in renderToClipboard

--- packages/components/src/Actions/CopyToClipboard.tsx
+++ packages/components/src/Actions/CopyToClipboard.tsx
@@ -1,9 +1,9 @@
 import { notification, Space } from "antd"
-import { useCallback, useState } from "react"
+import { ReactNode, useCallback, useState } from "react"
 import { CopyToClipboard as ReactCopyToClipboard } from "react-copy-to-clipboard"
 import { ContextAction } from "./ContextAction"
 
-export const CopyToClipboard = ({ content, title, successMessage }: { content: string; title?: string; successMessage?: string; }) => {
+export const CopyToClipboard = ({ content, title, successMessage }: { content: string; title?: ReactNode; successMessage?: string; }) => {
   const [isCopied, setIsCopied] = useState(false)
 
   const handleCopy = useCallback(() => {
@@ -13,7 +13,7 @@ export const CopyToClipboard = ({ content, title, successMessage }: { content: s
 
   return (
     <Space size={"small"}>
-      <span>{title || content}</span>
+      {(title && typeof title !== "string") ? title : <span>{title || content}</span>}
       <ReactCopyToClipboard text={content} children={<ContextAction tooltip="Copy to clipboard" type="copy" iconColor={isCopied ? "success" : undefined} />} onCopy={handleCopy} />
     </Space>
   )
