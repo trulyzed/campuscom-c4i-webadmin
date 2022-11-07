00c8b6c8b68d2a97c05929345f31574e59cd0b26
zayed <zayed@sgcsoft.net>
2022-11-07 09:22:21 +0600

    Fix crash in user preference data binding

--- apps/admin/src/Hook/useBrandingTitle.tsx
+++ apps/admin/src/Hook/useBrandingTitle.tsx
@@ -4,7 +4,7 @@ import { UserDataContext } from "@packages/components/lib/Context/UserDataContex
 export const useBrandingTitle = () => {
   const { userData } = useContext(UserDataContext)
   const primaryCourseProvider = useMemo(() => {
-    const defaultStoreId = userData?.preferences["default_store"]?.id
+    const defaultStoreId = userData?.preferences?.["default_store"]?.id
     const contextStores = Array.isArray(userData?.context) ? (userData?.context)?.find?.(i => i.type === "Store")?.values : []
     return contextStores?.find(j => j.id === defaultStoreId)?.primary_course_provider
   }, [userData])
