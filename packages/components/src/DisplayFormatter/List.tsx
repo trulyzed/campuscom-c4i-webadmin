b1f1cafb6339d95ff57d74e503a227988f87936a
zayed <zayed@sgcsoft.net>
2023-02-20 12:05:45 +0600

    Enhance list display renderer. Can emphasize, limit items.

--- packages/components/src/DisplayFormatter/List.tsx
+++ packages/components/src/DisplayFormatter/List.tsx
@@ -3,15 +3,22 @@ import { List as AntList, Tag } from 'antd'
 interface IListProps {
   data: string[]
   showInTags?: boolean
+  max?: number
+  emphasize?: string[]
+  emphasizeColor?: string
+  emphasizeTitle?: string
 }
 
-export const List = ({ data, showInTags }: IListProps) => {
+export const List = ({ data, showInTags, emphasize, max, emphasizeColor, emphasizeTitle }: IListProps) => {
+  const remaining = max ? data.length - max : undefined
   return (
     showInTags ?
       <div>
-        {data.map((d, idx) => (
-          <Tag key={`${d}__${idx}`}>{d}</Tag>
-        ))}
+        {(max ? data.slice(0, max) : data).map((d, idx) => {
+          const doEmphasize = emphasize?.includes(d)
+          return <Tag title={(doEmphasize && emphasizeTitle) ? `${d} ${emphasizeTitle}` : d} key={`${d}__${idx}`} color={doEmphasize ? (emphasizeColor || '#783392') : undefined}>{d}</Tag>
+        })}
+        {remaining ? <Tag title={`${remaining} more`}>+{remaining} More</Tag> : null}
       </div>
       : <AntList
         dataSource={data}
