aefdd9b861d024d7996111f794d7f256cdc6d465
zayed <zayed@sgcsoft.net>
2023-02-21 17:45:11 +0600

    Fix bug of list display renderer

--- packages/components/src/DisplayFormatter/List.tsx
+++ packages/components/src/DisplayFormatter/List.tsx
@@ -10,15 +10,15 @@ interface IListProps {
 }
 
 export const List = ({ data, showInTags, emphasize, max, emphasizeColor, emphasizeTitle }: IListProps) => {
-  const remaining = max ? data.length - max : undefined
+  const remaining = max && (data.length > max) ? data.length - max : undefined
   return (
     showInTags ?
-      <div>
-        {(max ? data.slice(0, max) : data).map((d, idx) => {
+      <div style={remaining ? { margin: "10px 0", maxWidth: "50rem", display: "flex", flexWrap: "wrap", gap: "5px" } : undefined}>
+        {(remaining ? data.slice(0, -remaining) : data).map((d, idx) => {
           const doEmphasize = emphasize?.includes(d)
-          return <Tag title={(doEmphasize && emphasizeTitle) ? `${d} ${emphasizeTitle}` : d} key={`${d}__${idx}`} color={doEmphasize ? (emphasizeColor || '#783392') : undefined}>{d}</Tag>
+          return <Tag style={{ margin: 0 }} title={(doEmphasize && emphasizeTitle) ? `${d} ${emphasizeTitle}` : d} key={`${d}__${idx}`} color={doEmphasize ? (emphasizeColor || '#783392') : undefined}>{d}</Tag>
         })}
-        {remaining ? <Tag title={`${remaining} more`}>+{remaining} More</Tag> : null}
+        {remaining ? <Tag style={{ margin: 0 }} title={`${remaining} more`} color={"#0077B3"}>+{remaining} More</Tag> : null}
       </div>
       : <AntList
         dataSource={data}
