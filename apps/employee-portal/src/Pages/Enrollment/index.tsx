b199f2c161cf3b59091aad56186b1f681c6f097b
zayed <zayed@sgcsoft.net>
2023-02-21 11:57:33 +0600

    Update configs

--- apps/employee-portal/src/Pages/Enrollment/index.tsx
+++ apps/employee-portal/src/Pages/Enrollment/index.tsx
@@ -1,11 +1,14 @@
 import { SearchPage } from "@packages/components/lib/Page/SearchPage/SearchPage"
+import { renderAmount } from "@packages/components/lib/ResponsiveTable"
 import { getEnrollmentListTableColumns } from "~/TableSearchMeta/Enrollment/EnrollmentListTableColumns"
 import { EnrollmentSearchMeta } from "~/TableSearchMeta/Enrollment/EnrollmentSearchMeta"
 
 export const List = () => {
   return (
     <SearchPage
-      title={"Enrollments"}
+      title={`Enrollments- Balance(${renderAmount(0)})`}
+      mainTitle={"Enrollments"}
+      searchTitle={"Enrollments- Filter"}
       meta={EnrollmentSearchMeta}
       tableProps={{
         ...getEnrollmentListTableColumns(),
--- apps/employee-portal/src/Pages/Skill/index.tsx
+++ apps/employee-portal/src/Pages/Skill/index.tsx
@@ -1,6 +1,5 @@
 import { SearchPage } from "@packages/components/lib/Page/SearchPage/SearchPage"
 import { renderBoolean } from "@packages/components/lib/ResponsiveTable"
-import { QueryConstructor } from "@packages/services/lib/Api/Queries/AdminQueries/Proxy"
 import { SkillQueries } from "@packages/services/lib/Api/Queries/EmployeePortalQueries/Skills"
 import { getSkillListTableColumns } from "~/TableSearchMeta/Skill/SkillListTableColumns"
 import { SkillSearchMeta } from "~/TableSearchMeta/Skill/SkillSearchMeta"
@@ -20,17 +19,7 @@ export const List = () => {
           render: renderBoolean,
           sorter: (a: any, b: any) => a.acquired - b.acquired,
         }],
-        searchFunc: QueryConstructor((params) => SkillQueries.getPaginatedList(params).then(resp => {
-          return {
-            code: 200,
-            data: [
-              { id: "1dba9373-399e-43df-aafa-b29a14a38153", name: 'aws', skill_type: 'technology', acquired: true },
-              { id: "1dba9373-399e-43df-aafa-b29a14a38f53", name: 'javascript', skill_type: 'technology', acquired: false },
-            ],
-            error: false,
-            success: true
-          }
-        }), [SkillQueries.getList]),
+        searchFunc: SkillQueries.getPaginatedList,
       }}
     />
   )
